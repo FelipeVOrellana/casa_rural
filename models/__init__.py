4aa2feac9de50433b704e7f9f368727898524bbd
felipo <felipe.orellana.robles@gmail.com>
2025-02-27 16:55:01 +0100

    Mensaje descriptivo del commit

--- models/__init__.py
+++ models/__init__.py
@@ -5,3 +5,16 @@ from . import huesped
 from . import estancia
 from . import factura
 from . import habitacion
+from . import canalcomunicacion
+from . import empleado
+from . import equipo
+from . import extra
+from . import incidencia
+from . import mensaje
+from . import pago
+from . import proveedor
+from . import reserva
+from . import servicio
+from . import tareamantenimiento
+from . import tiposervicio
+from . import tarifa
